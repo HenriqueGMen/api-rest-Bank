bd13813517a840db50cbca0f5cabf9c31a407cbf
HenriqueGMen <henrimeen24@gmail.com>
2024-04-02 00:03:17 -0300

    desacoplando estruturas de rotas

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,10 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const server = fastify()
 
-server.get('/home', async () => {
-  const transactions = await knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      tittle: 'Transactions teste',
-      amount: 1000,
-    })
-    .returning('*')
-
-  return transactions
-})
+server.register(transactionsRoutes)
 
 server
   .listen({
